b265d6764cee51beae44bb14748a7aea50794bfd
Christoph Berger <christophberger@users.noreply.github.com>
2016-03-16 22:51:51 +0100

    Minor changes in the doc parts.

--- goweave.go
+++ goweave.go
@@ -1,56 +1,60 @@
-// # goweave
-//
-// goweave creates an HTML file from a Go source file, rendering comments and
-// code side-by-side. Comments can use Markdown formatting as accepted by the
-// MarkdownCommon() method of the BlackFriday library; see below for details.
-//
-// Basically, goweave is meant to be the "weave" part of Literate Programming for Go.
-//
-/* Options:
+/*
+# goweave
+
+goweave creates an HTML file from a Go source file, rendering comments and
+code side-by-side. Comments can use Markdown formatting as accepted by the
+MarkdownCommon() method of the BlackFriday library; see below for details.
+
+Goweave is meant to work like "weave" in [Literate Programming]
+(https://en.wikipedia.org/wiki/Literate_programming). Unlike in Literate Programming,
+no "tangle" counterpart is required, as the source document is already a
+valid Go source file, ready to be `go install`'ed.
+
+Options:
 
 * `-resdir=<dir>`: Resource directory, defaults to the go get directory of goweave.
 * `-outdir=<dir>`: Output directory. Defaults to the current directory.
-* `-csspath=<path>`: Output path for the CSS file.
-* `-inline`: Include the CSS into the HTML file.
+* `-csspath=<path>`: Output path for the CSS file. Defaults to the current directory.
+* `-inline`: Include the CSS into the HTML file. Does not work with `-bare`.
 * `-md`: Generate Markdown output rather than HTML.
 * `-bare`: Only generate the body part of the HTML document.
- */
-// goweave is based on the wonderful [docgo](https://github.com/dhconnelly/docgo)
-// project by Daniel Connelly. Although I shuffled much of the code
-// around, added new code, removed some, and finally ended up with substantial
-// changes to the resulting behavior, docgo saved me a lot--a LOT!--of time as
-// it had all the groundworks already done for me.
-//
-// docgo in turn is a [Go](http://golang.org) implementation of [Jeremy Ashkenas]
-// (http://github.com/jashkenas)'s [docco] (http://jashkenas.github.com/docco/),
-// a literate-programming-style documentation generator.
-//
-// Comments are processed by [Markdown]
-// (http://daringfireball.net/projects/markdown) using [Russ Ross]
-// (http://github.com/russross)'s [BlackFriday]
-// (http://github.com/russross/blackfriday) library, and code is
-// syntax-highlighted using [litebrite](http://dhconnelly.github.com/litebrite),
-// a Go syntax highlighting library.
-//
-// Optionally you can generate a Markdown document instead of HTML. In this case,
-// you need to provide your own CSS that matches the output of your Markdown
-// renderer.
-// Also ensure your Markdown renderer is able to process "```go" code fences correctly.
-//
-//
-// goweave is copyright 2016 by Christoph Berger. All rights reserved.
-// This source code is governed by a BSD-style license that can be found in
-// the `LICENSE.txt` file.
-//
-// License files for godoc, litebrite, blackfriday, and the CopyFile function from
-// github.com/pkg/fileutils/copy.go:
-//
-// * LICENSE_godoc.md
-// * LICENSE_litebrite.md
-// * LICENSE_blackfriday.txt
-// * LICENSE_CopyFile.txt
 
-// - - -
+goweave is based on the wonderful [docgo](https://github.com/dhconnelly/docgo)
+project by Daniel Connelly. Although I shuffled much of the code
+around, added new code, removed some, and finally ended up with substantial
+changes to the resulting behavior, docgo saved me a lot--a LOT!--of time as
+it had all the groundworks already done for me.
+
+docgo in turn is a [Go](http://golang.org) implementation of [Jeremy Ashkenas]
+(http://github.com/jashkenas)'s [docco] (http://jashkenas.github.com/docco/),
+a literate-programming-style documentation generator.
+
+Comments are processed by [Markdown] (http://daringfireball.net/projects/markdown)
+using [Russ Ross] (http://github.com/russross)'s [BlackFriday]
+(http://github.com/russross/blackfriday) library, and code is
+syntax-highlighted using [litebrite](http://dhconnelly.github.com/litebrite),
+a Go syntax highlighting library.
+
+Optionally you can generate a Markdown document instead of HTML. In this case,
+you need to provide your own CSS that matches the output of your Markdown
+renderer.
+Also ensure your Markdown renderer is able to process "```go" code fences correctly.
+
+
+goweave is copyright 2016 by Christoph Berger. All rights reserved.
+This source code is governed by a BSD-style license that can be found in
+the `LICENSE.txt` file.
+
+Parts of the code are copyright 2012 by Daniel Connelly. See `LICENSE_godoc`.
+
+License files for litebrite, blackfriday, and the CopyFile function from
+github.com/pkg/fileutils/copy.go:
+
+* LICENSE_litebrite.md
+* LICENSE_blackfriday.txt
+* LICENSE_CopyFile.txt
+*/
+
 // ## Imports and globals
 
 package main
