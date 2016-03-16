3972a31a622e3790191fcdacb29fc024b012d8ac
Christoph Berger <christophberger@users.noreply.github.com>
2016-03-16 22:37:55 +0100

    Added: Comments now can also be enclosed in /* */ for better readability. Fixed: litebrite ignores leading whitespace of code snippets. A new func saves the leading whitespace for re-joining it later. Improved: CSS (code and doc alignment, padding/margin)

--- goweave.go
+++ goweave.go
@@ -6,15 +6,15 @@
 //
 // Basically, goweave is meant to be the "weave" part of Literate Programming for Go.
 //
-// Options:
-//
-// * `-resdir=<dir>`: Resource directory, defaults to the go get directory of goweave.
-// * `-outdir=<dir>`: Output directory. Defaults to the current directory.
-// * `-csspath=<path>`: Output path for the CSS file.
-// * `-inline`: Include the CSS into the HTML file.
-// * `-md`: Generate Markdown output rather than HTML.
-// * `-bare`: Only generate the body part of the HTML document.
-//
+/* Options:
+
+* `-resdir=<dir>`: Resource directory, defaults to the go get directory of goweave.
+* `-outdir=<dir>`: Output directory. Defaults to the current directory.
+* `-csspath=<path>`: Output path for the CSS file.
+* `-inline`: Include the CSS into the HTML file.
+* `-md`: Generate Markdown output rather than HTML.
+* `-bare`: Only generate the body part of the HTML document.
+ */
 // goweave is based on the wonderful [docgo](https://github.com/dhconnelly/docgo)
 // project by Daniel Connelly. Although I shuffled much of the code
 // around, added new code, removed some, and finally ended up with substantial
@@ -35,7 +35,7 @@
 // Optionally you can generate a Markdown document instead of HTML. In this case,
 // you need to provide your own CSS that matches the output of your Markdown
 // renderer.
-// Also ensure your Markdown renderer is able to process "````````go" tags correctly.
+// Also ensure your Markdown renderer is able to process "```go" code fences correctly.
 //
 //
 // goweave is copyright 2016 by Christoph Berger. All rights reserved.
@@ -50,6 +50,7 @@
 // * LICENSE_blackfriday.txt
 // * LICENSE_CopyFile.txt
 
+// - - -
 // ## Imports and globals
 
 package main
@@ -72,18 +73,24 @@ import (
 )
 
 var (
-	style       string
-	templ       *template.Template                // html template for generated docs
-	match       = regexp.MustCompile(`^\s*//\s?`) // pattern for extracted comments
-	outdir      = flag.String("outdir", ".", "output directory for html & css")
-	resdir      = flag.String("resdir", ".", "directory containing CSS and templates")
-	csspath     = flag.String("csspath", "", "relative path to CSS file, for use with the <link> element")
-	md          = flag.Bool("md", false, "generate Markdown document (default: HTML)")
-	bare        = flag.Bool("bare", false, "generate the HTML body only")
-	inline      = flag.Bool("inline", false, "generate inline CSS")
-	cssfilename = "goweave.css"
-	tplfilename = "goweave.templ"
-	pkg         = "github.com/christophberger/goweave" // for locating the resources if not specified
+	style            string
+	templ            *template.Template // html template for generated docs
+	commentPtrn      = `^\s*//\s?`
+	commentStartPtrn = `^\s*/\*\s?`
+	commentEndPtrn   = `\s?\*/\s*$`
+	comment          = regexp.MustCompile(commentPtrn)      // pattern for single-line comments
+	commentStart     = regexp.MustCompile(commentStartPtrn) // pattern for /* comment delimiter
+	commentEnd       = regexp.MustCompile(commentEndPtrn)   // pattern for */ comment delimiter
+	allCommentDelims = regexp.MustCompile(commentPtrn + "|" + commentStartPtrn + "|" + commentEndPtrn)
+	outdir           = flag.String("outdir", ".", "output directory for html & css")
+	resdir           = flag.String("resdir", ".", "directory containing CSS and templates")
+	csspath          = flag.String("csspath", "", "relative path to CSS file, for use with the <link> element")
+	md               = flag.Bool("md", false, "generate Markdown document (default: HTML)")
+	bare             = flag.Bool("bare", false, "generate the HTML body only")
+	inline           = flag.Bool("inline", false, "generate inline CSS")
+	cssfilename      = "goweave.css"
+	tplfilename      = "goweave.templ"
+	pkg              = "github.com/christophberger/goweave" // for locating the resources if not specified
 )
 
 // ## Generating documentation
@@ -129,34 +136,65 @@ func GenerateDocs(title, src string) (result string) {
 
 // ## Processing sections
 
+// Determine if the current line belongs to a comment region. A comment region
+// is either a comment line (starting with `//`) or a `/*...*/` comment section.
+func commentFinder() func(string) bool {
+	commentSectionInProgress := false
+	return func(line string) bool {
+		if comment.FindString(line) != "" {
+			// "//" Comment line found.
+			return true
+		}
+		if commentStart.FindString(line) != "" {
+			// Found the start `/*` of a comment section.
+			// Set a flag to remember this next time.
+			commentSectionInProgress = true
+			return true
+		}
+		if commentEnd.FindString(line) != "" {
+			// End `*/` of a comment section. Clear the flag.
+			commentSectionInProgress = false
+			return true
+		}
+		if commentSectionInProgress {
+			// We are currently within a `/*...*/` section.
+			return true
+		}
+		if len(line) == 0 {
+			// An empty line outside a `/*...*/` section is not a comment line.
+			return false
+		}
+		return false
+	}
+}
+
 // Split the source into sections, where each section contains a comment group
-// (consecutive leading-line // comments) and the code that follows that group.
+// and the code that follows that group.
 func extractSections(source string) []*section {
 	sections := make([]*section, 0)
 	current := new(section)
+	isInComment := commentFinder()
 
 	for _, line := range strings.Split(source, "\n") {
-		// When a candidate comment line is found, first test if we're currently
-		// in a Code group. If so, switch to a new section. Add the comment line
-		// to the section's Doc group.
-		if match.FindString(line) != "" {
+		// Determine if the line is a comment line, or the start of a `/*...*/` comment section.
+		if isInComment(line) {
+			// If currently in a Code group, switch to a new section.
 			if current.Code != "" {
 				sections = append(sections, current)
 				current = new(section)
 			}
-			// Strip out the comment delimiters and add the line to the
+			// Strip out any comment delimiter and add the line to the
 			// current doc section.
-			current.Doc += match.ReplaceAllString(line, "") + "\n"
+			current.Doc += allCommentDelims.ReplaceAllString(line, "") + "\n"
 		} else {
-			// No comment line found, so add the current line to the Code group.
+			// add the current line to the Code group.
 			current.Code += line + "\n"
 		}
 	}
-
 	return append(sections, current)
 }
 
-// Join sections into a single string
+// Join sections into a single string.
 func joinSections(sections []*section) (res string) {
 	for _, s := range sections {
 		res += s.Doc
@@ -174,12 +212,22 @@ func markdownComments(sections []*section) {
 	}
 }
 
+// litebrite eats leading whitespace when fed with code snippets.
+// To address this, splitLeadingWs splits the code into leading whitespace
+// and the rest, to be re-joined after highlighting.
+func splitLeadingWs(s string) (string, string) {
+	code := strings.TrimLeft(s, "\t ")
+	return s[:strings.Index(s, code)], code
+}
+
 // Apply syntax highlighting to each section's code.
 func highlightCode(sections []*section) {
 	h := litebrite.Highlighter{"operator", "ident", "literal", "keyword", "comment"}
 	for i := range sections {
-		if strings.Trim(strings.TrimSpace(sections[i].Code), "\n") != "" {
-			sections[i].Code = h.Highlight(sections[i].Code)
+		s := sections[i].Code
+		if strings.TrimSpace(strings.Trim(s, "\n")) != "" {
+			ws, code := splitLeadingWs(s)
+			sections[i].Code = ws + h.Highlight(code)
 		} else {
 			sections[i].Code = "" // make empty Code *really* empty
 		}
@@ -189,7 +237,9 @@ func highlightCode(sections []*section) {
 // Put the code into Markdown code fences
 func markdownCode(sections []*section) {
 	for i := range sections {
-		sections[i].Code = "\n```go\n" + sections[i].Code + "```\n"
+		if sections[i].Code != "\n" {
+			sections[i].Code = "\n```go\n" + sections[i].Code + "```\n"
+		}
 	}
 }
 
